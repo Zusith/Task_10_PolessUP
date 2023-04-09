a8d6eb340beaf9a8d8786f1c7aeeaafbdf1d004f
Kartech <kartech222@outlook.com>
2023-04-09 23:21:11 +0300

    Добавьте файлы проекта.

---
+++ Task_10_PolessUP/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int r = 3;
+int n = 5;
+Console.WriteLine(SumFirstNElementsInGroup(a, r, n));
+
+int SumFirstNElementsInGroup(int a, int r, int n)
+{
+    int result = 0;
+    for (int numberenter = 0; numberenter < n; numberenter++)
+    {
+        result += a + r * numberenter;
+    }
+    return result;
+}
+    
+
